3172483c286f30e3ae49cd0d6a70a0dad87745c0
kunalrupani <kunalrupani@gmail.com>
2017-04-28 16:41:31 -0700

    Initial creation

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'KKKKKK',
+    FB_VERIFY_TOKEN: 'KKKKK',
+    API_AI_CLIENT_ACCESS_TOKEN: 'KKKKK',
+    FB_APP_SECRET: 'KKKKK',
+    SERVER_URL: "https://pointylabs.herokuapp.com/askrupanibot/webhook",
 };
