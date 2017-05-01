3ffa62f353da766d45812db7903bcc42524b70a0
kunalrupani <kunalrupani@gmail.com>
2017-04-30 21:50:45 -0700

    Creation of askrupanibot.js

---
+++ helperfunctions/askrupanibot.js
@@ -0,0 +1,186 @@
+//Functions
+function receivedMessage(event) {
+
+	var senderID = event.sender.id;
+	var recipientID = event.recipient.id;
+	var timeOfMessage = event.timestamp;
+	var message = event.message;
+
+	if (!sessionIds.has(senderID)) {
+		sessionIds.set(senderID, uuid.v1());
+	}
+	console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+	console.log(JSON.stringify(message));
+
+	var isEcho = message.is_echo;
+	var messageId = message.mid;
+	var appId = message.app_id;
+	var metadata = message.metadata;
+
+	// You may get a text or attachment but not both
+	var messageText = message.text;
+
+	if (isEcho) {
+		handleEcho(messageId, appId, metadata);
+		return;
+	} 
+	if (messageText) {
+		//send message to api.ai
+		sendToApiAi(senderID, messageText);
+	} 
+}
+
+// Send received message to API.AI
+
+function sendToApiAi(sender, text) {
+
+	sendTypingOn(sender);
+	let apiaiRequest = apiAiService.textRequest(text, {
+		sessionId: sessionIds.get(sender)
+	});
+
+	apiaiRequest.on('response', (response) => {
+		if (isDefined(response.result)) {
+			handleApiAiResponse(sender, response);
+		}
+	});
+
+	apiaiRequest.on('error', (error) => console.error(error));
+	apiaiRequest.end();
+}
+
+function handleApiAiResponse(sender, response) {
+	console.log("************Received Response from API AI**************");
+	console.log(JSON.stringify(response));
+
+	let responseText = response.result.fulfillment.speech;
+
+	sendTypingOff(sender);
+
+	if (responseText == '' ) {
+		//api ai could not evaluate input.
+		console.log('Unknown query' + response.result.resolvedQuery);
+		sendTextMessage(sender, "Dude, I have no clue what you are saying. Try again...");
+	}  else {
+		sendTextMessage(sender, responseText);
+	}
+}
+
+
+function sendTextMessage(recipientId, text) {
+	var messageData = {
+		recipient: {
+			id: recipientId
+		},
+		message: {
+			text: text
+		}
+	}
+	callSendAPI(messageData);
+	console.log("************End Request**************");
+
+}
+
+
+
+//https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-echo
+//This callback will occur when a message has been sent by your page
+
+function handleEcho(messageId, appId, metadata) {
+	// Just logging message echoes to console
+	console.log("Received echo for message %s and app %d with metadata %s", messageId, appId, metadata);
+}
+
+function receivedPostback(event) {
+	var senderID = event.sender.id;
+	var recipientID = event.recipient.id;
+	var timeOfPostback = event.timestamp;
+
+	// The 'payload' param is a developer-defined field which is set in a postback 
+	// button for Structured Messages. 
+	var payload = event.postback.payload;
+
+	switch (payload) {
+		default:
+			//unindentified payload
+			sendTextMessage(senderID, "I'm not sure what you want. Can you be more specific?");
+			break;
+
+	}
+
+	console.log("Received postback for user %d and page %d with payload '%s' " +
+		"at %d", senderID, recipientID, payload, timeOfPostback);
+
+}
+
+/*
+ * Call the Send API. The message data goes in the body. If successful, we'll 
+ * get the message id in a response 
+ *
+ */
+function callSendAPI(messageData) {
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: config.FB_PAGE_TOKEN
+		},
+		method: 'POST',
+		json: messageData
+
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var recipientId = body.recipient_id;
+			var messageId = body.message_id;
+
+			if (messageId) {
+				console.log("Successfully sent message with id %s to recipient %s",
+					messageId, recipientId);
+
+			} else {
+				console.log("Successfully called Send API for recipient %s",
+					recipientId);
+			}
+
+		} else {
+			console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+		}
+	});
+}
+
+
+//Send indicator to FB Messager about the users typing action
+function sendTypingOn(recipientId) {
+	var messageData = {
+		recipient: {
+			id: recipientId
+		},
+		sender_action: "typing_on"
+	};
+
+	callSendAPI(messageData);
+}
+function sendTypingOff(recipientId) {
+	var messageData = {
+		recipient: {
+			id: recipientId
+		},
+		sender_action: "typing_off"
+	};
+
+	callSendAPI(messageData);
+}
+
+function isDefined(obj) {
+	if (typeof obj == 'undefined') {
+		return false;
+	}
+
+	if (!obj) {
+		return false;
+	}
+
+	return obj != null;
+}
+
+
+module.exports = {receivedMessage};
