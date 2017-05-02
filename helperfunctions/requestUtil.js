ec463c7f7211dab26d71ef2015066b530e231ecb
kunalrupani <kunalrupani@gmail.com>
2017-05-02 13:52:31 -0700

    .

--- helperfunctions/requestUtil.js
+++ helperfunctions/requestUtil.js
@@ -1,4 +1,7 @@
-
+/*
+ * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
 var https = require('https');
 
 /**
@@ -7,6 +10,40 @@ var https = require('https');
  * @param {callback} callback
  */
 function getUserData(accessToken, callback) {
+  var options = {
+    host: 'graph.microsoft.com',
+    path: '/v1.0/me',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer ' + accessToken
+    }
+  };
+
+  https.get(options, function (response) {
+    var body = '';
+    response.on('data', function (d) {
+      body += d;
+    });
+    response.on('end', function () {
+      var error;
+      if (response.statusCode === 200) {
+        callback(null, JSON.parse(body));
+      } else {
+        error = new Error();
+        error.code = response.statusCode;
+        error.message = response.statusMessage;
+        // The error body sometimes includes an empty space
+        // before the first character, remove it or it causes an error.
+        body = body.trim();
+        error.innerError = JSON.parse(body).error;
+        callback(error, null);
+      }
+    });
+  }).on('error', function (e) {
+    callback(e, null);
+  });
 }
 
 /**
@@ -16,6 +53,54 @@ function getUserData(accessToken, callback) {
  * @param {callback} callback
  */
 function postSendMail(accessToken, mailBody, callback) {
+  var outHeaders = {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + accessToken,
+    'Content-Length': mailBody.length
+  };
+  var options = {
+    host: 'graph.microsoft.com',
+    path: '/v1.0/me/microsoft.graph.sendMail',
+    method: 'POST',
+    headers: outHeaders
+  };
+
+  // Set up the request
+  var post = https.request(options, function (response) {
+    var body = '';
+    response.on('data', function (d) {
+      body += d;
+    });
+    response.on('end', function () {
+      var error;
+      if (response.statusCode === 202) {
+        callback(null);
+      } else {
+        error = new Error();
+        error.code = response.statusCode;
+        error.message = response.statusMessage;
+        // The error body sometimes includes an empty space
+        // before the first character, remove it or it causes an error.
+        body = body.trim();
+        error.innerError = JSON.parse(body).error;
+        // Note: If you receive a 500 - Internal Server Error
+        // while using a Microsoft account (outlok.com, hotmail.com or live.com),
+        // it's possible that your account has not been migrated to support this flow.
+        // Check the inner error object for code 'ErrorInternalServerTransientError'.
+        // You can try using a newly created Microsoft account or contact support.
+        callback(error);
+      }
+    });
+  });
+
+  // write the outbound data to it
+  post.write(mailBody);
+  // we're done!
+  post.end();
+
+  post.on('error', function (e) {
+    callback(e);
+  });
 }
 
 exports.getUserData = getUserData;
