0e5ea57a0b767466b9dfbb81c90aa0e23e7f9483
kunalrupani <kunalrupani@gmail.com>
2017-05-02 13:55:50 -0700

    .

--- helperfunctions/requestUtil.js
+++ helperfunctions/requestUtil.js
@@ -1,7 +1,4 @@
-/*
- * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
+
 var https = require('https');
 
 /**
