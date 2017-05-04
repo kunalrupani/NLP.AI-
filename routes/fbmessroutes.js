382b367b0935847557d506221cf5c7032f0796f2
kunalrupani <kunalrupani@gmail.com>
2017-05-04 10:05:23 -0700

    .

--- routes/fbmessroutes.js
+++ routes/fbmessroutes.js
@@ -12,48 +12,48 @@ const {receivedMessage} = require('../helperfunctions/askrupanibot');
 router.get('/', function (req, res) {
   // check for token
   console.log("Received a root request for askRupaniBot");
-  res.redirect('/webhook');
+  //res.redirect('/webhook');
 });
 
-// For Facebook verification
-router.get('/webhook', function (req, res) {
-	console.log("request");
-	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN) {
-		res.status(200).send(req.query['hub.challenge']);
-	} else {
-		console.error("Failed validation. Make sure the validation tokens match.");
-		res.sendStatus(403);
-	}
-})
+// // For Facebook verification
+// router.get('/webhook', function (req, res) {
+// 	console.log("request");
+// 	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN) {
+// 		res.status(200).send(req.query['hub.challenge']);
+// 	} else {
+// 		console.error("Failed validation. Make sure the validation tokens match.");
+// 		res.sendStatus(403);
+// 	}
+// })
 
-// Facebook Callbacks - all of them are POST requests
-router.post('/webhook', function (req, res) {
-	var data = req.body;
-	console.log("************New Request**************");
-	console.log(JSON.stringify(data));
+// // Facebook Callbacks - all of them are POST requests
+// router.post('/webhook', function (req, res) {
+// 	var data = req.body;
+// 	console.log("************New Request**************");
+// 	console.log(JSON.stringify(data));
 
-	// Make sure this is a page subscription
-	if (data.object == 'page') {
-		// Iterate over each entry
-		// There may be multiple if batched
-		data.entry.forEach(function (pageEntry) {
-			var pageID = pageEntry.id;
-			var timeOfEvent = pageEntry.time;
+// 	// Make sure this is a page subscription
+// 	if (data.object == 'page') {
+// 		// Iterate over each entry
+// 		// There may be multiple if batched
+// 		data.entry.forEach(function (pageEntry) {
+// 			var pageID = pageEntry.id;
+// 			var timeOfEvent = pageEntry.time;
 
-			// Iterate over each messaging event
-			pageEntry.messaging.forEach(function (messagingEvent) {
-			  if (messagingEvent.message) {
-					receivedMessage(messagingEvent);
-				} else if (messagingEvent.postback) {
-					receivedPostback(messagingEvent);
-				} else {
-					console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-				}
-			});
-		});
+// 			// Iterate over each messaging event
+// 			pageEntry.messaging.forEach(function (messagingEvent) {
+// 			  if (messagingEvent.message) {
+// 					receivedMessage(messagingEvent);
+// 				} else if (messagingEvent.postback) {
+// 					receivedPostback(messagingEvent);
+// 				} else {
+// 					console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+// 				}
+// 			});
+// 		});
 
-		// Assume all went well.
-		// You must send back a 200, within 20 seconds
-		res.sendStatus(200);
-	}
-});
+// 		// Assume all went well.
+// 		// You must send back a 200, within 20 seconds
+// 		res.sendStatus(200);
+// 	}
+// });
