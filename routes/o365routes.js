d8604f31bf23596b55925ae720247afaa32342c9
kunalrupani <kunalrupani@gmail.com>
2017-05-04 16:51:06 -0700

    .

--- routes/o365routes.js
+++ routes/o365routes.js
@@ -104,7 +104,7 @@ router.post('/', function (req, res) {
 
 router.get('/calendar/view', (req,rsp) => {
 
-request('www.google.com' , function (error, response, body) {
+request('http://www.google.com' , function (error, response, body) {
   console.log("Hello calendar ***********************");
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
