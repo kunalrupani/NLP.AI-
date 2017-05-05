dd7160e7f70a41d324d29498b39b28518250d321
kunalrupani <kunalrupani@gmail.com>
2017-05-05 16:19:53 -0700

    .

--- routes/googleroutes.js
+++ routes/googleroutes.js
@@ -59,7 +59,7 @@ router.get('/loginfirst', function (req, res) {
     console.log("Hello calendar ***********************");
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+  //  console.log('body:', body); // Print the HTML for the Google homepage.
     });
 
  });
