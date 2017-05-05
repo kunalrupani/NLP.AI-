00999193486fccbedf116efdfa33404119a43d03
kunalrupani <kunalrupani@gmail.com>
2017-05-04 17:09:38 -0700

    .

--- routes/o365routes.js
+++ routes/o365routes.js
@@ -103,7 +103,7 @@ router.post('/', function (req, res) {
 
 
 router.get('/calendar/view', (req,rsp) => {
-
+console.log('REQ.COOKIES.ACCESS_TOKEN', req.cookies.ACCESS_TOKEN_CACHE_KEY);
   var headers = {
     'Content-Type': 'application/json',
     Authorization: 'Bearer ' + req.cookies.ACCESS_TOKEN_CACHE_KEY,
