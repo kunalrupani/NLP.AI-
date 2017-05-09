ecdf1f8f89b3e99bbfe6d63c06f85917a4871ad8
kunalrupani <kunalrupani@gmail.com>
2017-05-09 09:44:31 -0700

    .

--- routes/googleroutes.js
+++ routes/googleroutes.js
@@ -110,7 +110,7 @@ router.get('/listcalendars', function (req, res) {
     console.log("Hello calendar ***********************");
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //  console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log('body:', body); // Print the HTML for the Google homepage.
     });
 
 
