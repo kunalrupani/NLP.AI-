65ca6d119fcb65af691cf95c2c1302968cc581a0
kunalrupani <kunalrupani@gmail.com>
2017-05-11 11:54:14 -0700

    .

--- routes/googleroutes.js
+++ routes/googleroutes.js
@@ -156,8 +156,6 @@ router.get('/listevents', function (req, res) {
    Oauth2Client.find({}, (err, oauth2clients)=>{
     oauth2Client1 = oauth2clients[0].oauth2_client; 
      var DefaultTransporter = {};
-     oauth2Client1.transporter = DefaultTransporter;
-     oauth2Client1.setCredentials(oauth2Client.credentials);
      console.log('oauth2clients object FROM DB ********************************',oauth2Client1 );
    }
   );
