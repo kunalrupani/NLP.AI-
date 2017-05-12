5a4fd7ab5eca9be5cb1a0c9eb3e5c1f64ed38962
kunalrupani <kunalrupani@gmail.com>
2017-05-11 17:30:32 -0700

    .

--- routes/googleroutes.js
+++ routes/googleroutes.js
@@ -158,7 +158,7 @@ router.get('/listevents', function (req, res) {
 
    Oauth2Client.find({}, (err, oauth2clients)=>{
 
-    oauth2Client1 = JSON.parse(oauth2clients[0].oauth2_client); 
+    oauth2Client1 = oauth2clients[0].oauth2_client; 
    
      console.log('oauth2clients object FROM DB ********************************',oauth2Client1 );
    }
@@ -171,7 +171,7 @@ router.get('/listevents', function (req, res) {
    console.log('oauth2clients object FROM memory ********************************',oauth2Client );
 
    calendar.events.list({
-    auth: oauth2Client1,
+    auth: JSON.parse(oauth2Client1),
     timeMin: (new Date()).toISOString(),
     maxResults: 3,
     singleEvents: true,
