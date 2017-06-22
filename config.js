0439fda3b4e5d110c17d52ed6ce10407f6344c3e
kunalrupani <kunalrupani@gmail.com>
2017-06-22 13:05:41 -0700

    .

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    FB_PAGE_TOKEN: 'EAADhHztV0xMBABYRUFlC3XoBqyBT9ZBdSyzZCoaZAeDDB2o5ZAe4Hiym7A3r7EZAPKJAypyjfi6zZCNfBZC6ajQaa1eOdvcyPvCNZByQiSPCdph1bhyCgmYSalDR7ap5VGjLqs4M5FeJQIn8dnRsZBGeCdPk8OTcsZBDZBG8Xm98JRjlwZDZD',
-    FB_VERIFY_TOKEN: 'KunalRupaniTest',
-    API_AI_CLIENT_ACCESS_TOKEN: 'd9a7ab8690bc4358a8c43cc41708f122',
-    FB_APP_SECRET: 'ac50f10418de5bf0f34c2fa8aaeba4ea',
-    SERVER_URL: "https://pointylabs.herokuapp.com/askrupanibot/webhook",
-};
